ea3d1d0a4cbcf07670523fb91dc05f40dcaaa4f0
Thompson <brandon.thompson@fluke.com>
2019-08-12 18:02:13 -0700

    Replaced magic numbers with const variables, and reordered global variables for the sake of clarity

--- MIscellaneous Files/Icarus-Main.ino
+++ MIscellaneous Files/Icarus-Main.ino
@@ -1,12 +1,7 @@
+#include <SparkFunMMA8452Q.h>
 #include <math.h>
-#include <algorithm>
-#include <array>
 #include "Adafruit_GFX.h"
 #include "Adafruit_PCD8544.h"
-#include <SparkFunMMA8452Q.h>
-
-
-using namespace std;
 
 Adafruit_PCD8544 display = Adafruit_PCD8544(SS, D2, D3);                                                            //initializes the display object for the LCD screen
 MMA8452Q accel;                                                                                                     //initializes the accelerometer object
@@ -19,34 +14,21 @@ const int tempButton = D5;                                                      
 const int maxButton = D6;                                                                                           //this input button switches the LCD screen to display the temperatures
 const int thermocouplePin = WKP;
 
-const int numValues = 11;
-const int medianLocation = 5;
-const int factorRound = 10;
-const double supplyVoltage = 3.3;                                                                                         //the supply voltage for the different compentents is 3.3 volts
-const int adcLevel = 4095;                                                                                           //as the ADC is a 12-bit ADC, has 0-4095 discrete levels of values
-const double adcRange = -1024;
-
-
-double values[numValues];
-double sortedValues[numValues];
-int arrIndex = 0;
-double total = 0;
-double median = 0;
+double supplyVoltage = 3.3;                                                                                         //the supply voltage for the different compentents is 3.3 volts
+int adcLevel = 4095;                                                                                                //as the ADC is a 12-bit ADC, has 0-4095 discrete levels of values
 double ambientTemp = 0;
 double thermistorTemp = 0.0;
 double irradiance = 0.0;
 double theta = 0.0;                                                                                                 //the degree value for the Y-axis of the Icarus instrument
+double adcRange = -1024;
 int degreeRange = 90;
 double maxAngle = 0.0;
 double maxIrradiance = 0.0;
-int screen = 1;
 
 bool wifiFlag = false;
 bool mainScreen = true;
 bool tempScreen = false;
 bool maxScreen = false;
-enum menuLists {MAIN = 1, TEMPERATURE, MAX};
-
 
 SYSTEM_MODE(SEMI_AUTOMATIC);                                                                                        //enables manual control of wifi on or off
 
@@ -74,10 +56,7 @@ void setup()   {
     display.display();                                                                                              //displays all text in the buffer and clears the buffer
     delay(1000);
     display.clearDisplay();
-
-    for (int thisValue = 0; thisValue < numValues; thisValue++) {
-        values[thisValue] = 0;
-    }
+  
 }
 
 void loop() {
@@ -85,17 +64,49 @@ void loop() {
     irradiance = getIrradiance();
     thermistorTemp = getThermistorTemp(getThermistorVoltage());
     ambientTemp = getAmbientTemp();
-    theta = accel.x / adcRange * degreeRange * -1;                                                      //scales the ADC value read from accelerometer to an angle measurement in positive, times -1 to account for orientation
     
-    irradiance = round(irradiance * factorRound) / factorRound;                                                                       //rounds the degrees to the nearest tenth
-    theta = round(theta * factorRound) / factorRound;
-    ambientTemp = round(ambientTemp * factorRound) / factorRound;
-    thermistorTemp = round(thermistorTemp * factorRound) / factorRound;
+    if (wifiFlag == true) {
+        Particle.publish("Thermistor_Temperature", String(thermistorTemp, 2));                                      //stores the thermistor temperature variable in the cloud
+        Particle.publish("Ambient_Temperature", String(ambientTemp,2));                                             //stores the ambient temperature variable in the cloud
+        Particle.publish("Irradiance", String(irradiance,2));                                                       //stores the irradiance variable in the cloud
+        Particle.publish("Incline", String(theta,2));                                                               //sores the incline degree in the cloud
+    }
     
-    setScreen();
+    theta = accel.x / adcRange * degreeRange;                                                                       //scales the ADC value read from accelerometer to an angle measurement in positive
+    irradiance = round(irradiance * 10) / 10;                                                                       //rounds the degrees to the nearest tenth
+    theta = round(theta * 10) / 10;
+    ambientTemp = round(ambientTemp * 10) / 10;
+    thermistorTemp = round(thermistorTemp * 10) / 10;
     
+    String screen = "";
+    
+    if (digitalRead(mainButton) == 0) {                                                                             //these if/else if conditionals check for if a button is pressed, and if so 
+        screen = "main";
+    }
+    else if (digitalRead(tempButton) == 0) {
+        screen = "temp";
+    }
+    else if (digitalRead(maxButton) == 0) {
+        screen = "max";
+    }
+    
+    setScreen(screen);
+    
+    if (mainScreen == true) {
+        display.println("Degrees: " + String(theta, 2));                                                            //truncates the extraneos zeroes in the float, so as to not imply a greater degree of precision
+        display.println();
+        display.println(String(irradiance, 2) + "  W/m^2");
+    }
+    else if (tempScreen == true) {
+        display.println("Temp: " + String(ambientTemp, 2));
+        display.println();
+        display.println("Therm: " + String(thermistorTemp, 2));
+    }
+    else if (maxScreen == true) {
+        findMax();
+    }
     display.display();
-    delay(250);
+    delay(1000);
     display.clearDisplay();
 }
 
@@ -123,12 +134,12 @@ double getThermistorTemp(double thermistorVoltage) {
   int voltDivResistor = 10000;                                                                                      //our resistror used in the voltage divider is a 10k Ohm resistor
   double thermB = 3428;                                                                                             //example value; 
   int refTemp = 25;                                                                                                 //the reference temperature is give nas 25 degrees Celsius
+
   double thermValue, thermResistance, temp;
-  double kelvinConversion = 273.15;
 
   thermResistance = (supplyVoltage - thermistorVoltage) / (thermistorVoltage / voltDivResistor);
                                                                                                                     //gets the thermistor resistance by using the 
-  temp = thermB * (refTemp + kelvinConversion) / (log(thermResistance / voltDivResistor) * (refTemp + kelvinConversion) + thermB) - kelvinConversion;
+  temp = thermB * (refTemp + 273.15) / (log(thermResistance / voltDivResistor) * (refTemp + 273.15) + thermB) - 273.15;
   temp = round(temp * 10) / 10;                                                                                     //this rounds the temp value to one decimal place
   return temp;
 }
@@ -136,7 +147,7 @@ double getThermistorTemp(double thermistorVoltage) {
 //reads the analog input from the TMP36 analog sensor, and returns the temperature in Celsius
 double getAmbientTemp() {
   double analogTempValue = analogRead(ambientOutput);
-  return ((analogTempValue * supplyVoltage / adcLevel) / .03 - 50);                                                               //this is the temperature equation for the TMP36 sensor
+  return ((analogTempValue * 3.3 / 4096) / .03 - 50);                                                               //this is the temperature equation for the TMP36 sensor
 }
 
 //gets the voltage reading from the thermistor
@@ -148,48 +159,43 @@ double getThermistorVoltage() {
 double getShortCurrent() {
   double shuntResistance = 0.08;
   double gain = 500.0;
-
-  total -= values[arrIndex];
-  double cellVoltage = analogRead(refCellOutput) * supplyVoltage / adcLevel;                                        //reads analog reading and converts ADC value to voltage using max volts and the 12-bit value
-  
-  values[arrIndex] = cellVoltage;
-  total += cellVoltage;
-  arrIndex++;
-  if (arrIndex >= numValues) {
-      arrIndex = 0;
-  }
-  
-  copy(begin(values), end(values), begin(sortedValues));
-  sort(begin(sortedValues), end(sortedValues));                                                                                 //the built in sort from  <algorithm> is O(n * log n) complexity, and sorts from the beginning of the array to the end
-  
-  return ((sortedValues[medianLocation] / gain) / shuntResistance);
+  double cellVoltage = analogRead(refCellOutput) * 3.3 / 4096;                                                      //reads analog reading and converts ADC value to voltage using max volts and the 12-bit value
+  return ((cellVoltage / gain) / shuntResistance);
 }
 
 //finds the max irradiance and angle associated with that irradiance when rotating the instrument about the x-axis for 5 seconds
 void findMax() {
     display.println("Please rotate Icarus around its X-axis in 5...");
     display.display();
-    delay(4000);
+    delay(3000);
     display.clearDisplay();
-    
-    for (int i = 4; i > 0; i--) {
-        display.println("" + String(i) + "...");
-        display.display();
-        delay(1000);
-        display.clearDisplay();
-    }
+    display.println("4...");
+    display.display();
+    delay(1000);
+    display.clearDisplay();
+    display.println("3...");
+    display.display();
+    delay(1000);
+    display.clearDisplay();
+    display.println("2...");
+    display.display();
+    delay(1000);
+    display.clearDisplay();
+    display.println("1...");
+    display.display();
+    delay(1000);
+    display.clearDisplay();
     display.println("Calculating...");
     display.display();
-    display.clearDisplay();
     
     for (int i = 0; i < 10; i++) {
         irradiance = getIrradiance();
         theta = accel.x / adcRange * degreeRange;                                                                       //scales the ADC value read from accelerometer to an angle measurement in positive
         
-        irradiance = round(irradiance * factorRound) / factorRound;                                                                       //rounds the degrees to the nearest tenth
-        theta = round(theta * factorRound) / factorRound;
+        irradiance = round(irradiance * 10) / 10;                                                                       //rounds the degrees to the nearest tenth
+        theta = round(theta * 10) / 10;
         
-        if (irradiance > maxIrradiance) {                                                                               //compares the values read with the current, and saves as new max and angle if the values are greate
+        if (irradiance > maxIrradiance) {
             maxIrradiance = irradiance;
             maxAngle = theta;
         }
@@ -197,57 +203,28 @@ void findMax() {
         delay(250);
     }
     
-    display.println("Max: " + String(maxIrradiance, 0));
-    display.println("Angle: " + String(maxAngle, 0));
+    display.println("Max: " + String(maxIrradiance, 2));
+    display.println("Angle: " + String(maxAngle, 2));
     display.display();
     delay(10000);
     display.clearDisplay();
-    screen = MAIN;
-    setScreen();
+    setScreen("main");
 }
 
-//compares the string parameter passed in, and depending on the value will set the boolean flags to correspond with the button pressed
-void setScreen() {
-    bool mainScreen = false;
-    bool tempScreen = false;
-    bool maxScreen = false;
-    
-    if (0 == digitalRead(mainButton)) {                                                                             //these if/else if conditionals check for if a button is pressed, and if so 
-        screen = MAIN;
+void setScreen(String screen) {
+    if (screen == "main") {
+        mainScreen = true;
+        tempScreen = false;
+        maxScreen = false;
     }
-    else if (0 == digitalRead(tempButton)) {
-        screen = TEMPERATURE;
+    else if (screen == "temp") {
+        mainScreen = false;
+        tempScreen = true;
+        maxScreen = false;
     }
-    else if (0 == digitalRead(maxButton)) {
-        screen = MAX;
-    }
-   
-    switch (screen) {                                       //if screen is MAIN (1), display's main screen info; if TEMP(2), temperature info
-        case MAIN:
-            mainScreen = true;
-            display.println("Degrees: " + String(theta, 0));                                                            //truncates the extraneos zeroes in the float, so as to not imply a greater degree of precision
-            display.println();
-            display.println(String(irradiance, 0) + "  W/m^2");
-            break;
-        case TEMPERATURE:
-            tempScreen = true;
-            display.println("Temp: " + String(ambientTemp, 0) + '\370');
-            display.println();
-            display.println("Therm: " + String(thermistorTemp, 0) + '\370');
-            break;
-        case MAX:
-            maxScreen = true;
-            findMax();
-            break;
-    }
-}
-
-//checks if wifiFlag is true (enabled); if so, publishes data to particle cloud storage for access
-void publishData() {
-     if (wifiFlag == true) {
-        Particle.publish("Thermistor_Temperature", String(thermistorTemp, 2));                                      //stores the thermistor temperature variable in the cloud
-        Particle.publish("Ambient_Temperature", String(ambientTemp, 1));                                             //stores the ambient temperature variable in the cloud
-        Particle.publish("Irradiance", String(irradiance,1));                                                       //stores the irradiance variable in the cloud
-        Particle.publish("Incline", String(theta,1));                                                               //sores the incline degree in the cloud
+    else if (screen == "max") {
+        mainScreen = false;
+        tempScreen = false;
+        maxScreen = true;
     }
 }
